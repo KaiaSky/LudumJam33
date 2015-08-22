9bff7acb4ade20b26e00bcad4f4faa3ddcb9caec
Keon Skyfire <keonskyfire@gmail.com>
2015-08-22 16:34:23 -0700

    Button

--- src/com/github/keon3141/ludumdare33/gui/Button.java
+++ src/com/github/keon3141/ludumdare33/gui/Button.java
@@ -10,7 +10,10 @@ import com.github.keon3141.ludumdare33.gameloop.World;
 public class Button extends GUIElement{
 
 	public boolean hovered = false;
-	public boolean clicked = true;
+	public boolean clicked = false;
+	public boolean clicking = false;
+	public float clickedTime = 1f;
+	public float currentTime = 1f;
 	
 	public Button(float x, float y, ArrayList<Animation> animations) {
 		super(x, y, animations);
@@ -23,12 +26,34 @@ public class Button extends GUIElement{
 		if(this.rect.contains(in.getMouseX(), in.getMouseY()))
 		{
 			hovered = true;
-			if(in.isMousePressed(in.MOUSE_LEFT_BUTTON))
+			if(in.isMouseButtonDown(in.MOUSE_LEFT_BUTTON) && ! clicking)
 			{
 				clicked = true;
 			}
 		}
 		else{hovered = false;}
+		
+		if(clicked)
+		{
+			clicking = true;
+		}
+		if(clicking)
+		{
+			currentTime -= dt;
+			this.setCurrentAnimation(2);
+		}
+		if(currentTime < 0)
+		{
+			clicking = false;
+			currentTime = clickedTime;
+		}
+		if(hovered && !clicking)
+		{
+			this.setCurrentAnimation(1);
+		}else if (! hovered && ! clicking)
+		{
+			this.setCurrentAnimation(0);
+		}
 	}
 	
 	public boolean pollClicked()
